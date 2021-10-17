503f1766b7cbc6149d06af63823d009b5652f3b2
Kevin Malakoff <kmalakoff@gmail.com>
2021-10-17 10:12:31 -0700

    Moved types

---
+++ dist/esm/index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Create a newlinw iterator recognizing CR, LF, and CRLF using the Symbol.iterator interface
+ *
+ * @param string The string to iterate through
+ *
+ * ```typescript
+ * import newlineIterator from "newline-iterator";
+ *
+ * const iterator = newlineIterator("some\r\nstring\ncombination\r");
+ * const results = [];
+ * for (const line of iterator) results.push(line);
+ * console.log(results); // ["some", "string", "combination"];
+ * ```
+ */
+export default function newlineIterator(string: string): IterableIterator<string>;
