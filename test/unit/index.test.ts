dc7f28b1bc75dd4c733f690ce9f3ec1d4a17650c
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-27 00:12:25 -0700

    Update ts-dev-stack

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,54 +1,54 @@
-import assert from "assert";
-import newlineIterator from "newline-iterator";
+import assert from 'assert';
+import newlineIterator from 'newline-iterator';
 
-const hasIterator = typeof Symbol !== "undefined" && Symbol.iterator;
+const hasIterator = typeof Symbol !== 'undefined' && Symbol.iterator;
 
-describe("newline-iterator", function () {
-  describe("next", function () {
-    it("all values", function () {
-      const string = "some\r\nstring\ncombination\r";
+describe('newline-iterator', function () {
+  describe('next', function () {
+    it('all values', function () {
+      const string = 'some\r\nstring\ncombination\r';
       const iterator = newlineIterator(string);
 
-      assert.deepEqual(iterator.next(), { value: "some", done: false });
-      assert.deepEqual(iterator.next(), { value: "string", done: false });
-      assert.deepEqual(iterator.next(), { value: "combination", done: false });
+      assert.deepEqual(iterator.next(), { value: 'some', done: false });
+      assert.deepEqual(iterator.next(), { value: 'string', done: false });
+      assert.deepEqual(iterator.next(), { value: 'combination', done: false });
       assert.deepEqual(iterator.next(), { value: undefined, done: true });
     });
 
-    it("no end", function () {
-      const string = "some\r\nstring\ncombination";
+    it('no end', function () {
+      const string = 'some\r\nstring\ncombination';
       const iterator = newlineIterator(string);
-      assert.deepEqual(iterator.next(), { value: "some", done: false });
-      assert.deepEqual(iterator.next(), { value: "string", done: false });
-      assert.deepEqual(iterator.next(), { value: "combination", done: false });
+      assert.deepEqual(iterator.next(), { value: 'some', done: false });
+      assert.deepEqual(iterator.next(), { value: 'string', done: false });
+      assert.deepEqual(iterator.next(), { value: 'combination', done: false });
       assert.deepEqual(iterator.next(), { value: undefined, done: true });
     });
 
-    it("no breaks", function () {
-      const string = "somestringcombination";
+    it('no breaks', function () {
+      const string = 'somestringcombination';
       const iterator = newlineIterator(string);
-      assert.deepEqual(iterator.next(), { value: "somestringcombination", done: false });
+      assert.deepEqual(iterator.next(), { value: 'somestringcombination', done: false });
       assert.deepEqual(iterator.next(), { value: undefined, done: true });
     });
   });
 
   !hasIterator ||
-    describe("iterator", function () {
-      it("all values", function () {
-        const string = "some\r\nstring\ncombination\r";
+    describe('iterator', function () {
+      it('all values', function () {
+        const string = 'some\r\nstring\ncombination\r';
         const iterator = newlineIterator(string);
 
         const results = [];
         for (const line of iterator) results.push(line);
-        assert.deepEqual(results, ["some", "string", "combination"]);
+        assert.deepEqual(results, ['some', 'string', 'combination']);
       });
 
-      it("no breaks", function () {
-        const string = "somestringcombination";
+      it('no breaks', function () {
+        const string = 'somestringcombination';
         const iterator = newlineIterator(string);
         const results = [];
         for (const line of iterator) results.push(line);
-        assert.deepEqual(results, ["somestringcombination"]);
+        assert.deepEqual(results, ['somestringcombination']);
       });
     });
 });
