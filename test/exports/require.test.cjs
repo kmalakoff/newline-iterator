b4fec08bf28cc79e18b5443e4e1a3a0d059f5dd9
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-27 00:12:25 -0700

    Update ts-dev-stack

--- test/exports/require.test.cjs
+++ test/exports/require.test.cjs
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const assert = require("assert");
-const newlineIterator = require("newline-iterator");
+const assert = require('assert');
+const newlineIterator = require('newline-iterator');
 
-describe("exports .cjs", function () {
-  it("first newline", function () {
-    const iterator = newlineIterator("some\r\nstring\ncombination\r");
-    assert.deepEqual(iterator.next(), { value: "some", done: false });
+describe('exports .cjs', function () {
+  it('first newline', function () {
+    const iterator = newlineIterator('some\r\nstring\ncombination\r');
+    assert.deepEqual(iterator.next(), { value: 'some', done: false });
   });
 });
