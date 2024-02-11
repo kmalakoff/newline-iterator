4d5ddc98b371b6aac036b1c6bb3160993086e2c3
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-10 22:07:47 -0800

    Upgrade modules

--- docs/assets/search.js
+++ docs/assets/search.js
@@ -1,1 +1,3 @@
-window.searchData = JSON.parse("{\"kinds\":{\"64\":\"Function\"},\"rows\":[{\"kind\":64,\"name\":\"newlineIterator\",\"url\":\"functions/newlineIterator.html\",\"classes\":\"tsd-kind-function\"}],\"index\":{\"version\":\"2.3.9\",\"fields\":[\"name\",\"comment\"],\"fieldVectors\":[[\"name/0\",[0,2.877]],[\"comment/0\",[]]],\"invertedIndex\":[[\"newlineiterator\",{\"_index\":0,\"name\":{\"0\":{}},\"comment\":{}}]],\"pipeline\":[]}}");
+window.searchData = JSON.parse(
+  '{"kinds":{"64":"Function"},"rows":[{"kind":64,"name":"newlineIterator","url":"functions/newlineIterator.html","classes":"tsd-kind-function"}],"index":{"version":"2.3.9","fields":["name","comment"],"fieldVectors":[["name/0",[0,2.877]],["comment/0",[]]],"invertedIndex":[["newlineiterator",{"_index":0,"name":{"0":{}},"comment":{}}]],"pipeline":[]}}',
+);
