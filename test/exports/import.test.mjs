d8cc9ca24314f2ac117ff56a80e6dfe32329a8f8
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 21:56:47 -0800

    Migrate to Biome and update build configuration

--- test/exports/import.test.mjs
+++ test/exports/import.test.mjs
@@ -1,8 +1,8 @@
 import assert from 'assert';
 import newlineIterator from 'newline-iterator';
 
-describe('exports .mjs', function () {
-  it('first newline', function () {
+describe('exports .mjs', () => {
+  it('first newline', () => {
     const iterator = newlineIterator('some\r\nstring\ncombination\r');
     assert.deepEqual(iterator.next(), { value: 'some', done: false });
   });
