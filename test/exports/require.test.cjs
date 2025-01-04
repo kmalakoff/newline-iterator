f784a40e2a6da6310fc67528707e6351b1b7b450
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 21:56:47 -0800

    Migrate to Biome and update build configuration

--- test/exports/require.test.cjs
+++ test/exports/require.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
 const newlineIterator = require('newline-iterator');
 
-describe('exports .cjs', function () {
-  it('first newline', function () {
+describe('exports .cjs', () => {
+  it('first newline', () => {
     const iterator = newlineIterator('some\r\nstring\ncombination\r');
     assert.deepEqual(iterator.next(), { value: 'some', done: false });
   });
