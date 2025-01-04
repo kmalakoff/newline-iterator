f03e84bbdf971c06279b805b5245927e27415130
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 20:22:22 -0800

    Upgrade modules

--- test/exports/unpkg.min.test.cjs
+++ test/exports/unpkg.min.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
-const newlineIterator = require('newline-iterator/dist/umd/newline-iterator.min.js');
+const newlineIterator = require('newline-iterator/dist/umd/newline-iterator.min.cjs');
 
-describe('exports newline-iterator/dist/umd/newline-iterator.min.js', function () {
-  it('first newline', function () {
+describe('exports newline-iterator/dist/umd/newline-iterator.min.cjs', () => {
+  it('first newline', () => {
     const iterator = newlineIterator('some\r\nstring\ncombination\r');
     assert.deepEqual(iterator.next(), { value: 'some', done: false });
   });
--- test/exports/unpkg.test.cjs
+++ test/exports/unpkg.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
-const newlineIterator = require('newline-iterator/dist/umd/newline-iterator.js');
+const newlineIterator = require('newline-iterator/dist/umd/newline-iterator.cjs');
 
-describe('exports newline-iterator/dist/umd/newline-iterator.js', function () {
-  it('first newline', function () {
+describe('exports newline-iterator/dist/umd/newline-iterator.cjs', () => {
+  it('first newline', () => {
     const iterator = newlineIterator('some\r\nstring\ncombination\r');
     assert.deepEqual(iterator.next(), { value: 'some', done: false });
   });
