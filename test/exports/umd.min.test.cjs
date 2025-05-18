4f8e18d2d7196be947a2895b0377e1ac49ab3716
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-17 19:48:43 -0700

    Upgrade modules

--- test/exports/umd.min.test.cjs
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const newlineIterator = require('newline-iterator/dist/umd/newline-iterator.min.cjs');
-
-describe('exports newline-iterator/dist/umd/newline-iterator.min.cjs', () => {
-  it('first newline', () => {
-    const iterator = newlineIterator('some\r\nstring\ncombination\r');
-    assert.deepEqual(iterator.next(), { value: 'some', done: false });
-  });
-});
--- test/exports/umd.test.cjs
+++ test/exports/umd.test.cjs
@@ -1,7 +1,14 @@
 const assert = require('assert');
-const newlineIterator = require('newline-iterator/dist/umd/newline-iterator.cjs');
 
-describe('exports newline-iterator/dist/umd/newline-iterator.cjs', () => {
+let umd = null;
+try {
+  umd = require('newline-iterator/umd');
+} catch (_) {
+  umd = require('newline-iterator/dist/umd/newline-iterator.cjs');
+}
+const newlineIterator = typeof window !== 'undefined' ? window.newlineIterator : umd.default || umd;
+
+describe('exports umd', () => {
   it('first newline', () => {
     const iterator = newlineIterator('some\r\nstring\ncombination\r');
     assert.deepEqual(iterator.next(), { value: 'some', done: false });
